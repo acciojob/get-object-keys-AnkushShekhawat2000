b337d21075d49760769d1369b1a47bb9375368bc
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-29 01:39:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating the student object
+const student = {
+  name: "Ankush"
+};
+
+// Adding a property to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Testing the getKeys() method on the student object
+const keysArray = student.getKeys();
+console.log(keysArray); // Output: ["name"]
